032ad203abbf7614b01b5b4d0fc1fb943775d7cc
sayamss <sawaisayam@gmail.com>
2020-08-05 17:37:44 +0000

    Removed Unused Code

--- src/components/intro.js
+++ src/components/intro.js
@@ -2,22 +2,9 @@ import React, {useState} from 'react'
 import '../styles/intro.css'
 export default function Intro() {
 
-    const [skills, setSkills] = useState([
-        'Python',
-        'Javascript',
-        'C',
-        'C++',
-        'Reactjs',
-        'ARM Assembly',
-        'x86 Assembly',
-        'Reactjs',
-        'Angularjs',
-        'Django',
-        'Flask',
-    ])
     return (
         <div>
-            
+
         </div>
     )
 }
